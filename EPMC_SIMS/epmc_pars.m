ee920900c90e7b80d3770caa44cdef17ca9d5615
JustinPorter88 <jp88@rice.edu>
2022-08-28 20:10:27 -0500

    Removed input parameters and plots not used in paper

--- EPMC_SIMS/epmc_pars.m
+++ EPMC_SIMS/epmc_pars.m
@@ -33,233 +33,6 @@ switch pars_num
         inputpars.arcSettings.da = 0.04; % If displacements are near constant, then steps are 0.02 to start (since dA is scaled by b=0.5).
         inputpars.arcSettings.dsmax = 0.2;
         inputpars.arcSettings.dsmin = 0.001;
-
-        
-    case 1
-        
-        % Full / Best Model, TMD Abstract
-        
-        inputpars.Sys = 330e6; %Pa
-        inputpars.mu = 1e10;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 3; %mu + CEB
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'zte152'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = 'epmc_tmd_absV2_run1';
-        inputpars.load_initial = false;
-%         inputpars.prevInd = 
-%         inputpars.loadName = 'Results/
-
-        inputpars.arcSettings.da = 1e3;
-        inputpars.arcSettings.dsmax = 80e5;
-        inputpars.arcSettings.dsmin = 1e3;
-
-        
-    case 2
-        
-        % Without Mesoscale Model, TMD Abstract
-        
-        inputpars.Sys = 330e6; %Pa
-        inputpars.mu = 1e10;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 3; %mu + CEB
-        inputpars.mesoscale = false; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'zte152'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = 'epmc_tmd_absV2_run2';
-        inputpars.load_initial = false;
-%         inputpars.prevInd = 
-%         inputpars.loadName = 'Results/
-
-        inputpars.arcSettings.da = .5e3;
-        inputpars.arcSettings.dsmax = 80e5;
-        inputpars.arcSettings.dsmin = 1e2;
-
-        
-    case 3
-        
-        % Etsion Unloading, TMD Abstract
-        
-        inputpars.Sys = 330e6; %Pa
-        inputpars.mu = 1e10;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 3; %mu + CEB
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'ETSION';
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'zte152'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = 'epmc_tmd_absV2_run3';
-        inputpars.load_initial = false;
-%         inputpars.prevInd = 
-%         inputpars.loadName = 'Results/
-
-        inputpars.arcSettings.da = 1e3;
-        inputpars.arcSettings.dsmax = 80e5;
-        inputpars.arcSettings.dsmin = 1e3;
-
-        
-    case 4
-        
-        % Elastic Model, TMD Abstract
-        
-        inputpars.Sys = Inf; %Pa
-        inputpars.mu = 0.02;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 1; %mu 
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'zte152'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = 'epmc_tmd_absV2_run4d';
-        inputpars.load_initial = true;
-        inputpars.prevInd = 8;
-        inputpars.loadName = 'Results/epmc_tmd_absV2_run4c_iter8.mat';
-
-        inputpars.arcSettings.da = 4e6;
-        inputpars.arcSettings.dsmax = 5e7;
-        inputpars.arcSettings.dsmin = 5e5;
-
-        
-        
-        
-    case 5
-        
-        % Full + mu, TMD Abstract
-        
-        inputpars.Sys = 330e6; %Pa
-        inputpars.mu = 0.02;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 3; %mu + CEB
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'zte152'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = 'epmc_tmd_absV2_run5d';
-        inputpars.load_initial = true;
-        inputpars.prevInd = 78;
-        inputpars.loadName = 'Results/epmc_tmd_absV2_run5_iter78.mat';
-
-        inputpars.arcSettings.da = 5e6;
-        inputpars.arcSettings.dsmax = 5e7;
-        inputpars.arcSettings.dsmin = 5e5;
-
-        
-    case 6
-        
-        % Elastic + mu, Mesoscale = flat TMD Presentation
-        
-        inputpars.Sys = Inf; %Pa
-        inputpars.mu = 0.02;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 1; %mu + CEB
-        inputpars.mesoscale = false; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'zte152'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = 'epmc_tmd_absV2_run6d';
-        inputpars.load_initial = false;
-        inputpars.prevInd = 78;
-        inputpars.loadName = 'Results/epmc_tmd_absV2_run6d_iter*.mat';
-
-        inputpars.arcSettings.da = 5e6;
-        inputpars.arcSettings.dsmax = 5e7;
-        inputpars.arcSettings.dsmin = 5e5;
-        
-    case 7
-        
-        % Elastic Model, Comparison to RQNMA, Paper
-        
-        inputpars.Sys = Inf; %Pa
-        inputpars.mu = 0.02;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 1; %mu 
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.Et = 620e6;
-        
-        inputpars.useSphere = true;
-
-        inputpars.meshName = 'UROM232'; % 'zte152', 'zte588', 'zte588Jul21', 'zte588convgX' (X = 7, 8, 9, 10), 'UROM122', 'UROM232'
-        inputpars.meshNameRelative = ''; % '' or '_NOREL'
-        
-        inputpars.output_name = 'epmc_paper7';
-        inputpars.load_initial = false;
-%         inputpars.prevInd = 8;
-%         inputpars.loadName = 'Results/epmc_paper7.mat';
-
-        inputpars.arcSettings.da = 4e6;
-        inputpars.arcSettings.dsmax = 5e7;
-        inputpars.arcSettings.dsmin = 5e5;
-
-    case 8
-        
-        % Full / Best Model, IMAC 2021 Abstract
-        inputpars.Sys = 330e6; %Pa
-        inputpars.mu = 1e10;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 3; %mu + CEB
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.useSphere = true;
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'UROM232'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = sprintf('epmc_imac2021_run%u_v3', pars_num);
-        inputpars.load_initial = true;
-        inputpars.prevInd = 18;
-        inputpars.loadName = 'Results/epmc_imac2021_run8_v2_iter18';
-
-        inputpars.arcSettings.da = 4e5;
-        inputpars.arcSettings.dsmax = 64e6;
-        inputpars.arcSettings.dsmin = 1e3;
-
-    case 9
-        
-        % Just mu, IMAC 2021 Abstract
-        inputpars.Sys = 330e6; %Pa
-        inputpars.mu = 0.03;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 1; %mu only
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.useSphere = true;
-        inputpars.Et = 620e6;
-        
-        inputpars.meshName = 'UROM232'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = sprintf('epmc_imac2021_run%u_v3', pars_num);
-        inputpars.load_initial = false;
-%         inputpars.prevInd = 
-%         inputpars.loadName = 'Results/
-
-        inputpars.arcSettings.da = 4e7;
-        inputpars.arcSettings.dsmax = 4e8;
-        inputpars.arcSettings.dsmin = 1e3;
     
     case 10
         % Paper: Backbone 1 - Fully Elastic
@@ -452,32 +225,6 @@ switch pars_num
         inputpars.arcSettings.dsmax = 0.1;
         inputpars.arcSettings.dsmin = 0.001;
     
-    case 18
-        % Paper: Backbone 9 - SNL Properties
-        inputpars.Sys = 330e6; %Pa
-        inputpars.mu = 1e10;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        inputpars.sliptype = 5; %CEB+mu only w/ UTS CEB
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        inputpars.unloadModel = 'brake';
-        inputpars.useSphere = true;
-        inputpars.Et = 620e6;
-        inputpars.UTS = 713.5e6; % Pa
-        
-        inputpars.meshName = 'UROM232'; %'zte152', 'zte588', 'zte588Jul21'
-        
-        inputpars.output_name = sprintf('epmc_paper_run%u', pars_num);
-        inputpars.load_initial = false;
-%         inputpars.prevInd = 
-%         inputpars.loadName = 'Results/
-
-        inputpars.arcSettings.da = 0.04; % If displacements are near constant, then steps are 0.02 to start (since dA is scaled by b=0.5).
-        inputpars.arcSettings.dsmax = 0.2;
-        inputpars.arcSettings.dsmin = 0.001;
-    
-        
-    
     case 19
         % Paper: Backbone 10 - SNL Properties + erroded asperities
         
@@ -579,7 +326,7 @@ switch pars_num
    
     case 23
         
-        % Elastic Spheres, Mode 2
+        % Mode 2
         inputpars = epmc_pars(14);
         
         inputpars.output_name = sprintf('epmc_paper_run%u', pars_num);
@@ -596,7 +343,7 @@ switch pars_num
         
     case 24
         
-        % Elastic Spheres, Mode 2
+        % Mode 2
         inputpars = epmc_pars(11);
         
         inputpars.output_name = sprintf('epmc_paper_run%u', pars_num);
@@ -612,7 +359,7 @@ switch pars_num
         
     case 25
         
-        % Elastic Spheres, Mode 2
+        % Mode 2
         inputpars = epmc_pars(15);
         
         inputpars.output_name = sprintf('epmc_paper_run%u', pars_num);
@@ -628,7 +375,7 @@ switch pars_num
         
     case 26
         
-        % Elastic Spheres, Mode 2
+        % Mode 2
         inputpars = epmc_pars(19);
         
         inputpars.output_name = sprintf('epmc_paper_run%u', pars_num);
--- FJSIMS/rqnm_pars.m
+++ FJSIMS/rqnm_pars.m
@@ -34,87 +34,6 @@ switch pars_num
         
         inputpars.DeltaGamma = 5; % J/m^2
         
-    case 1 
-        inputpars.mu = 0.02;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        
-        % Aperity Function
-        inputpars.asp_fun = @ELLIPSOID_TAN_DECOUPLE; inputpars.Nqp_radius = 1; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_DECOUPLE; inputpars.Nqp_radius = 100; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_FIT_DECOUPLE; inputpars.Nqp_radius = 100; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_FIT_COUPLE; inputpars.Nqp_radius = 100; 
-        
-        inputpars.alpha = 0; % Rotation from x to small ellipsoid axis
-        inputpars.useSphere = true; % Convert ellipsoid properties to sphere
-        inputpars.Nqp_heights = 100;
-        
-        inputpars.meshName = 'zte588convg10'; % 'zte152', 'zte588', 'zte588Jul21', 'zte588convgX' (X = 7, 8, 9, 10), 'UROM122', 'UROM232'
-        inputpars.meshNameRelative = '_NOREL'; % '' or '_NOREL'
-
-        inputpars.output_name = 'Results/rqnm_paper1';
-        inputpars.load_initial = false;
-        
-        % RQNMA Solver Settings
-        inputpars.repeatLoop = 4;
-        inputpars.Nhp = 9;
-        
-    case 2 
-        inputpars.mu = 0.02;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        
-        % Aperity Function
-        inputpars.asp_fun = @ELLIPSOID_TAN_DECOUPLE; inputpars.Nqp_radius = 1; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_DECOUPLE; inputpars.Nqp_radius = 100; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_FIT_DECOUPLE; inputpars.Nqp_radius = 100; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_FIT_COUPLE; inputpars.Nqp_radius = 100; 
-        
-        inputpars.alpha = 0; % Rotation from x to small ellipsoid axis
-        inputpars.useSphere = true; % Convert ellipsoid properties to sphere
-        inputpars.Nqp_heights = 100;
-        
-        inputpars.meshName = 'UROM122'; % 'zte152', 'zte588', 'zte588Jul21', 'zte588convgX' (X = 7, 8, 9, 10), 'UROM122', 'UROM232'
-        inputpars.meshNameRelative = ''; % '' or '_NOREL'
-
-        inputpars.output_name = 'Results/rqnm_paper2';
-        inputpars.load_initial = false;
-        
-        % RQNMA Solver Settings
-        inputpars.repeatLoop = 4;
-        inputpars.Nhp = 9;
-         
-    case 3 
-        inputpars.mu = 0.02;
-        inputpars.ElasticModulus = 192.31e9; % Pa
-        inputpars.nu = 0.3;
-        
-        inputpars.mesoscale = true; % Multiplies ZTE heights;
-        
-        % Aperity Function
-        inputpars.asp_fun = @ELLIPSOID_TAN_DECOUPLE; inputpars.Nqp_radius = 1; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_DECOUPLE; inputpars.Nqp_radius = 100; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_FIT_DECOUPLE; inputpars.Nqp_radius = 100; 
-%         inputpars.asp_fun = @ELLIPSOID_IWAN_FIT_COUPLE; inputpars.Nqp_radius = 100; 
-        
-        inputpars.alpha = 0; % Rotation from x to small ellipsoid axis
-        inputpars.useSphere = true; % Convert ellipsoid properties to sphere
-        inputpars.Nqp_heights = 100;
-        
-        inputpars.meshName = 'UROM232'; % 'zte152', 'zte588', 'zte588Jul21', 'zte588convgX' (X = 7, 8, 9, 10), 'UROM122', 'UROM232'
-        inputpars.meshNameRelative = ''; % '' or '_NOREL'
-
-        inputpars.output_name = 'Results/rqnm_paper3';
-        inputpars.load_initial = false;
-        
-        % RQNMA Solver Settings
-        inputpars.repeatLoop = 4;
-        inputpars.Nhp = 9;
-    
     case 4 
         inputpars.mu = 0.01;
         inputpars.ElasticModulus = 192.31e9; % Pa
@@ -167,7 +86,6 @@ switch pars_num
         inputpars.mu = 0.9;
         inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
         
-    
     case 11 
         inputpars = rqnm_pars(4);
         inputpars.mu = 0.03;
@@ -194,46 +112,6 @@ switch pars_num
         
         inputpars.asp_fun = @ELLIPSOID_IWAN_FIT_COUPLE; inputpars.Nqp_radius = 100; 
         inputpars.alpha = pi/2; % Rotation from x to small ellipsoid axis
-        inputpars.useSphere = false; % Convert ellipsoid properties to sphere
-        
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-    
-    case 14 
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        
-        inputpars.asp_fun = @ELLIPSOID_TAN_DECOUPLE; inputpars.Nqp_radius = 1; 
-        inputpars.alpha = 0; % Rotation from x to small ellipsoid axis
-        inputpars.useSphere = true; % Convert ellipsoid properties to sphere
-        
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-    
-    case 15 
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        
-        inputpars.asp_fun = @ELLIPSOID_TAN_DECOUPLE; inputpars.Nqp_radius = 1; 
-        inputpars.alpha = 0; % Rotation from x to small ellipsoid axis
-        inputpars.useSphere = false; % Convert ellipsoid properties to sphere
-        
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-    
-    case 16 
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        
-        inputpars.asp_fun = @ELLIPSOID_TAN_DECOUPLE; inputpars.Nqp_radius = 1; 
-        inputpars.alpha = pi/4; % Rotation from x to small ellipsoid axis
-        inputpars.useSphere = false; % Convert ellipsoid properties to sphere
-        
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-    
-    case 17 
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        
-        inputpars.asp_fun = @ELLIPSOID_TAN_DECOUPLE; inputpars.Nqp_radius = 1; 
-        inputpars.alpha = -pi/4; % Rotation from x to small ellipsoid axis
         inputpars.useSphere = false; % Convert ellipsoid properties to sphere
         
         inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
@@ -266,74 +144,6 @@ switch pars_num
         inputpars.useSphere = false; % Convert ellipsoid properties to sphere
         inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
     
-    case 22
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-        
-        %%%% Center Gap
-        l = 0.121412; % length of interface
-        h = 50e-6; % height of shape
-        R = h/2 + l^2/8/h;
-        inputpars.overrideMesoscale = @(XY) sqrt(R^2 - XY(:, 1).^2);
-    
-    case 23
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-        
-        %%%% Center Gap
-        l = 0.121412; % length of interface
-        h = 100e-6; % height of shape
-        R = h/2 + l^2/8/h;
-        inputpars.overrideMesoscale = @(XY) sqrt(R^2 - XY(:, 1).^2);
-        
-        
-    case 24
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-        
-        %%%% Center Gap
-        l = 0.121412; % length of interface
-        h = 250e-6; % height of shape
-        R = h/2 + l^2/8/h;
-        inputpars.overrideMesoscale = @(XY) sqrt(R^2 - XY(:, 1).^2);
-        
-        
-    case 25
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-        
-        %%%% Center Gap
-        l = 0.121412; % length of interface
-        h = 50e-6; % height of shape
-        R = h/2 + l^2/8/h;
-        inputpars.overrideMesoscale = @(XY) -sqrt(R^2 - XY(:, 1).^2);
-        
-        
-    case 26
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-        
-        %%%% Center Gap
-        l = 0.121412; % length of interface
-        h = 250e-6; % height of shape
-        R = h/2 + l^2/8/h;
-        inputpars.overrideMesoscale = @(XY) -sqrt(R^2 - XY(:, 1).^2);
-       
-        
-    case 27
-        % Flat interface for comparison.
-        inputpars = rqnm_pars(4);
-        inputpars.mu = 0.03;
-        inputpars.output_name = sprintf('Results/rqnm_paper%u', pars_num);
-        
-        inputpars.mesoscale = false; % Multiplies ZTE heights;
-        
-        
     otherwise
         error('Did not supply valid parameter input');
 end
